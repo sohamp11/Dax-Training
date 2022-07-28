06ac43fcd10e8929d8b17a174db19414e5703bd5
Soham Patil <Soham.Patil@data-axle.com>
2022-07-28 10:23:35 +0530

    dot net assignment

---
+++ Dot Net/DotNet_Assign/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_Assign.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options): base(options)
+        {
+
+        }
+        public DbSet<Registration> Registration { get; set; }
+    }
+}
